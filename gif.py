232781abe5a996afdb3cfbdfb0f0937963a80c3a
Sai Sathvik Akash Sudagani <84318646+sat854@users.noreply.github.com>
2025-09-21 17:54:45 +0000

    added codebase for making gif

--- gif.py
+++ gif.py
@@ -1,2 +1,6 @@
 import imageio.v3 as iio
 
+files = ['ER1.png','ER2.png','ER3.png','ER4.png','ER5.png']
+images = [iio.imread(i) for i in files]
+iio.imwrite('team.gif', images ,duration = 200,loop = 0)
+
